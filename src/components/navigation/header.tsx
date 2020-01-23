b668df696f4dfdbc55f74ac16f624fc31357379c
RobinHoodie <rhellemans@laterpay.net>
2020-01-23 07:27:21 +0100

    Show which page you're currently on by marking active link

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,6 +1,7 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React, { Dispatch } from "react";
+import { globalHistory } from "@reach/router";
 
 import "./header.sass";
 import "./menu-item.sass";
@@ -13,18 +14,20 @@ interface HeaderProps {
   siteTitle: string;
 }
 
+const activeLinkClassIfPathIsActive = (currentPath: string, activePath: string): string => currentPath === activePath ? "header__link--active" : "";
+
 const Header = ({ layoutDispatch, siteTitle }: HeaderProps): JSX.Element => (
   <header className="header">
     <nav className="header__navigation-bar">
       <Brand siteTitle={siteTitle} />
       <Link
         to="/portfolio/"
-        className="header__link">
+        className={`header__link ${activeLinkClassIfPathIsActive("/portfolio/", globalHistory.location.pathname)}`}>
         Portfolio
       </Link>
       <Link
         to="/blog/"
-        className="header__link">
+        className={`header__link ${activeLinkClassIfPathIsActive("/blog/", globalHistory.location.pathname)}`}>
         Blog
       </Link>
       <button
